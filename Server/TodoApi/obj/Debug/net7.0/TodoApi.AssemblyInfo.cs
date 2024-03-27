77bd2d8ca9bba4b4a20b29d3477c45d3665573b4
tzippy-eisenstein <tzippy0898@gmail.com>
2024-03-28 01:05:33 +0200

    change

--- Server/TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
+++ Server/TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2dd8db580d052b6edaca2b5dc606b8d89d46bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e998b66626fa200242520cbe34772a2baafd0505")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
